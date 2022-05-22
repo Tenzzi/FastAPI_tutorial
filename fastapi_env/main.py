7f095c9e0a3a7e277a0173ee0c30606c97eb3d84
Tenzzi <07ja26@gmail.com>
2022-05-22 14:04:15 +0900

    fastapi tutorial

--- fastapi_env/main.py
+++ fastapi_env/main.py
@@ -5,6 +5,20 @@ fastAPI 사용법:
     3. 사용할 함수 만들기
     4. 함수 인스턴스 메서드 데코레이터로 감싸주기(경로 지정)
 """
+  """_summary_
+
+  터미널에서 실행하는 방법
+  uvicorn (파일 이름):(FastAPI 인스턴스) --reload(리로드 기능을 원할 시) 
+  
+  reload 기능이란? 
+    - ctrl + S로 저장만 하면 서버가 재시작되고 코드의 수정사항이 반영됨
+    
+    @app.get('/about')         -> get, post, delete 등 operation 
+    def about_page():
+    return {'data': 'This is about page'}  
+  """
+
+
 
 from fastapi import FastAPI
 
